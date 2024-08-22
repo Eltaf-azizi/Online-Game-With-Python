3326517d5415d263357f2c07072f54f4f8843f52
unknown <iltfhuss@gmail.com>
2024-08-22 23:24:07 +0500

    added code

--- round.py
+++ round.py
@@ -5,6 +5,13 @@ time, skips, drawing player and more.
 
 class Rounf(object):
     def init(self, word, playerdrawing, players):
+
+        """
+        init object
+        :param word: str
+        :param playerdrawing: Player
+        :param players: Player[]
+        """
         self.word = word
         self.playerdrawing = playerdrawing
         self.playerguessed = []
@@ -13,4 +20,19 @@ class Rounf(object):
         self.time = 0
 
     def guess(self, player , wrd):
+        """
+        :returns bool if player got guess correct
+        :param player: Player
+        :param wed: str
+        :return: bool
+        """
         return wrd == self.wrd
+    
+    def playerleft(self, player):
+        """
+        removes player that left from scores and list
+        :param player: Player
+        :return: None
+        """
+        if player in self.playerscores:
+            del playerscores[player]
