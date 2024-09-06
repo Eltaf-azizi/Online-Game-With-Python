d21a8373cfb9029c73e36ee49bbd1a16abe3f3f8
unknown <iltfhuss@gmail.com>
2024-09-06 21:10:42 +0500

    added code

--- board.py
+++ board.py
@@ -3,20 +3,46 @@ Stores the state of the deawing board.
 """
 
 class Board(object):
-    def __init__(self, rows,cols):
+    ROWS = COLS = 720
+    
+    def __init__(self):
+        """
+        init the board by creating empty board (all white pixels)
+        """
         self.data = self.createemptyboard()
 
     def update(self, x, y, color):
+        """
+        updates a singular of the pixel
+        :param x: int
+        :param y: int
+        :param color: (int, int, int)
+        :return: 
+        """
         self.data[y][x] = color
 
     def clear(self):
+        """
+        clears board to all white
+        :return: None
+        """
         self.data = self.createemptyboard()
 
     def create_empty_board(self):
         return [[{244, 244, 244} for _ in range(self.COLS) for _ in range(self.ROWS)]]
     
     def fill(self, x, y):
+        """
+        fills in a specific shape or data using recursion
+        :param x: int
+        :param y: int
+        :return: None
+        """
         pass
 
     def get_board(self):
+        """
+        gets the data of the board
+        :return: (int, int, int)[]
+        """
         return self.data
