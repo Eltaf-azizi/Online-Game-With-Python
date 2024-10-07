58a07698a6007066f1c059fdad07b0292dc7d043
unknown <iltfhuss@gmail.com>
2024-10-07 22:37:11 +0500

    added code

---
+++ client/top_bar.py
@@ -0,0 +1,52 @@
+"""
+Top bar displaying information about round
+"""
+import pygame
+
+
+class TopBar(object):
+    def __init__(self, x, y,width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.word = ""
+        self.round = 1
+        self.maxround = 8
+        self.roundfont = pygame.font.Sysfont("comicsans", 30)
+        self.BORDERTHICKNESS = 5
+
+
+
+    def draw(self, win):
+        pygame.draw.rect(win, (0, 0, 0), (self.x, self.y, self.width,self.height), self.BORDERTHICKNESS)
+
+        # draw round
+
+        txt = self.roundfont.render(f"Round {self.round} of {self.maxround}", 1, (0, 0, 0))
+        win.blit(txt, (self.x + 10, self.y + self.height/2 - txt.get_height()/2))
+
+        # draw underscores
+        txt = self.roundfont.render(TopBar.underscoretext(self.word), 1, (0, 0, 0))
+        win.blit(txt, (self.x + self.width/2 - txt.getwidth()/2, self.y + self.height/2 - txt.get_height()/2 + 10))
+
+
+
+    def underscoretext(text):
+        newstr = ""
+
+        for char in text:
+            if char in text:
+                newstr += ""
+            else:
+                newstr += " "
+
+        return newstr
+
+
+    def changeword(self, word):
+        self.word = word
+
+    
+    def changeround(self, rnd):
+        self.round = rnd
