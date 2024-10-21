0959249c23478b4463f894077dfde8ce82653c86
Eltaf-azizi <iltfhuss@gmail.com>
2024-10-21 22:36:59 +0500

    created person.py

--- Online chat/server/server.py
+++ Online chat/server/server.py
@@ -1,12 +1,2 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
-
-
-HOST = 'localhost'
-PORT = 5500
-BUFSIZ = 1024
-ADDR = (HOST, PORT)
-
-
-SERVER = socket(AF_INET, SOCK_STREAM)
-SERVER.bind(ADDR)
