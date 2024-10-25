ae9230c0345b4b23ab5eb28894e01556c6a97688
Eltaf-azizi <iltfhuss@gmail.com>
2024-10-25 20:17:53 +0500

    added code

--- Online chat/main.py
+++ Online chat/main.py
@@ -1,21 +1,37 @@
-from flask import Flask, render_template, url_for 
+from flask import Flask, render_template, url_for, redirect, session, NAME_KEY
 
 
 app = Flask(__name__)
-app.secret_key = "hello"
+app.secret_key = "hellomynameisEltafandyouwon'tguessthis"
 
 @app.route("/login")
 
 
+
 def login():
-    return render_template("")
+    return render_template("login.html")
 
 
+
+@app.route("/logout")
+
+def logout():
+    session.pop(NAME_KEY, None)
+    return redirect(url_for("login"))
+
+
+
+app.route("/")
 app.route("/home")
 
 
+
 def home():
-    return render_template("")
+    if NAME_KEY not in session:
+        return redirect(url_for("login"))
+
+    name = session[NAME_KEY]
+    return render_template("index.html")
 
 
 
