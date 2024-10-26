79c353161fb2db179cdfc482be201cdcc5c6076e
Eltaf-azizi <iltfhuss@gmail.com>
2024-10-26 22:34:01 +0500

    added code and index.js file

--- Online chat/website/main.py
+++ Online chat/website/main.py
@@ -4,12 +4,14 @@ from flask import Flask, render_template, url_for, redirect, session, NAME_KEY
 app = Flask(__name__)
 app.secret_key = "hellomynameisEltafandyouwon'tguessthis"
 
+
+
+"""
 @app.route("/login")
-
-
 
 def login():
     return render_template("login.html")
+    """
 
 
 
@@ -27,11 +29,15 @@ app.route("/home")
 
 
 def home():
-    if NAME_KEY not in session:
-        return redirect(url_for("login"))
+    # if NAME_KEY not in session:
+    #     return redirect(url_for("login"))
 
-    name = session[NAME_KEY]
+    # name = session[NAME_KEY]
     return render_template("index.html")
+
+
+def run():
+    print("clicked")
 
 
 
