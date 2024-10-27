4e0f96bacd015167e60d621e3942499530206f3c
Eltaf-azizi <iltfhuss@gmail.com>
2024-10-27 22:14:54 +0500

    added code and login.html file

--- Online chat/website/main.py
+++ Online chat/website/main.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for, redirect, session, NAME_KEY
+from flask import Flask, render_template, url_for, redirect, session, request
+from client import Client
 
+
+NAME_KEY = 'name'
 
 app = Flask(__name__)
 app.secret_key = "hellomynameisEltafandyouwon'tguessthis"
@@ -7,9 +10,13 @@ app.secret_key = "hellomynameisEltafandyouwon'tguessthis"
 
 
 
-@app.route("/login")
+@app.route("/login", methods=["POST", "GET"])
 
 def login():
+    if request.method == "POST":
+        session[NAME_KEY] = request.form["name"]
+        return redirect(url_for("name"))
+
     return render_template("login.html")
 
 
@@ -28,10 +35,10 @@ app.route("/home")
 
 
 def home():
-    # if NAME_KEY not in session:
-    #     return redirect(url_for("login"))
+    if NAME_KEY not in session:
+        return redirect(url_for("login"))
 
-    # name = session[NAME_KEY]
+
     return render_template("index.html")
 
 
