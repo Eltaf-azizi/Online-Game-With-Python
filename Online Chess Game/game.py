34f3cbbbe4486d9f04cdfba17f6cadeab16075da
Eltaf-azizi <iltfhuss@gmail.com>
2024-11-04 20:41:41 +0500

    added board.py

--- Online Chess Game/game.py
+++ Online Chess Game/game.py
@@ -11,3 +11,12 @@ b_knight = pygame.image.load(os.path.join("image", "black_knight.png"))
 b_pawn = pygame.image.load(os.path.join("image", "black_pawn.png"))
 b_queen = pygame.image.load(os.path.join("image", "black_queen.png"))
 b_rook = pygame.image.load(os.path.join("image", "black_rook.png"))
+
+
+
+w_bishop = pygame.image.load(os.path.join("image", "white_bishop.png"))
+w_king = pygame.image.load(os.path.join("image", "white_king.png"))
+w_knight = pygame.image.load(os.path.join("image", "white_knight.png"))
+w_pawn = pygame.image.load(os.path.join("image", "white_pawn.png"))
+w_queen = pygame.image.load(os.path.join("image", "white_queen.png"))
+w_rook = pygame.image.load(os.path.join("image", "white_rook.png"))
