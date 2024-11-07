c58f583909ae0aefb37d124fd5b80ff96f212ba0
Eltaf-azizi <iltfhuss@gmail.com>
2024-11-07 23:06:56 +0500

    added code

--- Online Chess Game/board.py
+++ Online Chess Game/board.py
@@ -1,8 +1,9 @@
 from piece import Bishop
-from piece import Bishop
-from piece import Bishop
-from piece import Bishop
-from piece import Bishop
+from piece import King
+from piece import Knight
+from piece import Pawn
+from piece import Queen
+from piece import Rook
 
 
 
@@ -18,8 +19,36 @@ class Board:
         print(self.board)
 
 
-        self.board[0][0]
-        self.board[0][1]
-        self.board[0][2]
-        self.board[0][3]
-        self.board[0][4]
+        self.board[0][0] = Bishop(0, 0, "b")
+        self.board[0][1] = King(0, 1, "b")
+        self.board[0][2] = Knight(0, 2, "b")
+        self.board[0][3] = Pawn(0, 3, "b")
+        self.board[0][4] = Queen(0, 4, "b")
+        self.board[0][5] = Rook(0, 5, "b")
+
+
+        self.board[1][0] = Pawn(0, 0, "b")
+        self.board[1][1] = Pawn(0, 1, "b")
+        self.board[1][2] = Pawn(0, 2, "b")
+        self.board[1][3] = Pawn(0, 3, "b")
+        self.board[1][4] = Pawn(0, 4, "b")
+        self.board[1][5] = Pawn(0, 5, "b")
+
+
+
+
+
+        self.board[7][0] = Bishop(0, 0, "w")
+        self.board[7][1] = King(0, 1, "w")
+        self.board[7][2] = Knight(0, 2, "w")
+        self.board[7][3] = Pawn(0, 3, "w")
+        self.board[7][4] = Queen(0, 4, "w")
+        self.board[7][5] = Rook(0, 5, "w")
+
+
+        self.board[6][0] = Pawn(0, 0, "w")
+        self.board[6][1] = Pawn(0, 1, "w")
+        self.board[6][2] = Pawn(0, 2, "w")
+        self.board[6][3] = Pawn(0, 3, "w")
+        self.board[6][4] = Pawn(0, 4, "w")
+        self.board[6][5] = Pawn(0, 5, "w")
